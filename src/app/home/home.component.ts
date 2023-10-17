e912bce10a25efbfa196ae700909117e93b5d948
smalhadid <smalhadid@gmail.com>
2023-10-17 15:39:56 -0400

    trying to fix token timeout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import fetchFromSpotify, { request } from "../../services/api";
+import fetchFromSpotify from "../../services/api";
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UserService } from "../user.service";
 import { Router } from "@angular/router";
@@ -8,8 +8,6 @@ import { Router } from "@angular/router";
 
 const AUTH_ENDPOINT =
   "https://nuod0t2zoe.execute-api.us-east-2.amazonaws.com/FT-Classroom/spotify-auth-token";
-
-  const TOKEN_KEY = "whos-who-access-token";
 
 @Component({
   selector: "app-home",
@@ -41,34 +39,25 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.authLoading = true;
-    const storedTokenString = localStorage.getItem(TOKEN_KEY);
-    if (storedTokenString) {
-      const storedToken = JSON.parse(storedTokenString);
-      if (storedToken.expiration > Date.now()) {
-        console.log("Token found in localstorage");
+    this.userService.getToken().subscribe({
+      next: response => {
+        const accessToken = response.access_token;
+        console.log('Access Token:', accessToken);
         this.authLoading = false;
-        this.token = storedToken.value;
-        this.loadGenres(storedToken.value);
-        return;
+        this.token = accessToken;
+        this.loadGenres(this.token);
+      },
+      error: error => {
+        console.error('Error:', error);
+        this.authLoading = false;
       }
-    }
-    console.log("Sending request to AWS endpoint");
-    request(AUTH_ENDPOINT).then(({ access_token, expires_in }) => {
-      const newToken = {
-        value: access_token,
-        expiration: Date.now() + (expires_in - 20) * 1000,
-      };
-      localStorage.setItem(TOKEN_KEY, JSON.stringify(newToken));
-      this.authLoading = false;
-      this.token = newToken.value;
     });
-
   }
 
   loadGenres = async (t: any) => {
     this.configLoading = true;
     const response = await fetchFromSpotify({
-      token: this.token,
+      token: t,
       endpoint: "recommendations/available-genre-seeds",
     });
     console.log(response);
